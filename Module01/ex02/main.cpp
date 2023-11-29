f9a8da58fe3a59d9593899e9d3170d64e2b92229
Rubens Teles Freire Junior <rteles-f@c2r7s1.42lisboa.com>
2023-11-29 14:13:35 +0000

    Module 01 ex02 done

--- Module01/ex02/main.cpp
+++ Module01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rteles-f <rteles-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 21:44:30 by rteles-f          #+#    #+#             */
-/*   Updated: 2023/05/08 21:57:54 by rteles-f         ###   ########.fr       */
+/*   Updated: 2023/11/29 14:11:49 by rteles-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 int	main(void)
 {
 	std::string	hi = "HI THIS IS BRAIN";
-	std::string	*pointer = &hi;
-	std::string	&reference = hi;
-	
+	std::string	*stringPTR = &hi;
+	std::string	&stringREF = hi;
+		
 	std::cout << "String Adress: " << &hi << std::endl;
-	std::cout << "Pointer Content: " << pointer << std::endl;
-	std::cout << "Reference Content: " << &reference << std::endl;
+	std::cout << "Pointer Content: " << stringPTR << std::endl;
+	std::cout << "Reference Content: " << &stringREF << std::endl;
 
 	std::cout << std::endl;
 
 	std::cout << "String Value: " << hi << std::endl;
-	std::cout << "Pointer Value: " << *pointer << std::endl;
-	std::cout << "Reference Value: " << reference << std::endl;
+	std::cout << "Pointer Value: " << *stringPTR << std::endl;
+	std::cout << "Reference Value: " << stringREF << std::endl;
 }
